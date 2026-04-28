16f59721196b110cac5d04b21f6ede9a98014905
lafayette-04 <minouche.us@gmail.com>
2026-04-29 00:26:25 +0200

    index.js

--- index.js
+++ index.js
@@ -1,38 +1,74 @@
-const express = require('express');
-const app = express();
-
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-res.send('Bot Discord actif');
-});
-
-app.listen(PORT, () => {
-console.log(`Serveur web actif sur ${PORT}`);
-});
-
-// ===== DISCORD BOT =====
-
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 
 const client = new Client({
 intents: [
 GatewayIntentBits.Guilds,
-GatewayIntentBits.GuildMessages,
-GatewayIntentBits.MessageContent
+GatewayIntentBits.GuildMessages
 ]
 });
 
-client.on('ready', () => {
+const CHANNEL_ID = "1496696155541864633";
+
+let sessionActive = false;
+
+client.once('ready', async () => {
 console.log(`Connecté en tant que ${client.user.tag}`);
 });
 
-client.on('messageCreate', message => {
+client.on('messageCreate', async (message) => {
 if (message.author.bot) return;
 
-if (message.content === '!ping') {
-message.reply('pong 🏓');
+const channel = await client.channels.fetch(CHANNEL_ID);
+
+// ▶️ START
+if (message.content === "!start") {
+if (sessionActive) {
+return message.reply("❌ Une session est déjà en cours");
+}
+
+sessionActive = true;
+message.reply("✅ Session lancée");
+
+loopSession(channel);
+}
+
+// ⏹ STOP
+if (message.content === "!stop") {
+sessionActive = false;
+message.reply("🛑 Session arrêtée");
 }
 });
+
+async function loopSession(channel) {
+if (!sessionActive) return;
+
+// 🟢 SESSION
+const embedStart = new EmbedBuilder()
+.setTitle("💎 SESSION ARTICLE")
+.setDescription("Poste ton article et like les autres ❤️\n⏱️ 10 minutes chrono\n🚀 Sois actif !")
+.setImage("https://i.ibb.co/6Jm36jvX/84-F407-FF-EB63-4-EB3-83-D9-553-A1-A1-B57-D6.png")
+.setColor("#ff7b00");
+
+await channel.send({ embeds: [embedStart] });
+
+setTimeout(async () => {
+if (!sessionActive) return;
+
+// 🔴 STOP
+const embedStop = new EmbedBuilder()
+.setTitle("🛑 SESSION STOP")
+.setDescription("Session terminée ❌")
+.setImage("https://i.ibb.co/j9mGMjDm/AE44-C3-D4-5-F52-4-D45-AE27-409-BDF00-D67-B.png")
+.setColor("#ff0000");
+
+await channel.send({ embeds: [embedStop] });
+
+// relance après 15 sec
+setTimeout(() => {
+if (sessionActive) loopSession(channel);
+}, 15000);
+
+}, 10 * 60 * 1000);
+}
 
 client.login(process.env.TOKEN);
